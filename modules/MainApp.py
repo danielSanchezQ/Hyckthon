ba29b80e515bf6217bcc98438087546335244e5f
Netwave <3danimanimal@gmail.com>
2015-11-18 04:26:10 +0800

    added ip ranges to hyckon tools, promoted intergaface to main app, removed prints form hythontools, added prints to mainapp

--- modules/MainApp.py
+++ modules/MainApp.py
@@ -3,22 +3,28 @@ __author__ = 'Netwave'
 
 import requests
 import nmap
+import re
 from pprint import pprint
 class HythonTools(object):
     def __init__(self):
         self.whoisinfo = None
         self.portScaner = nmap.PortScanner()
+        self.ips = None
 
     def whois(self, url):
         request_str = "http://api.hackertarget.com/whois/?q={}".format(url)
         response = requests.get(request_str)
         self.whoisinfo = response.content
-        pprint(self.whoisinfo.split("\n"))
         return self.whoisinfo
+
+    def loadIps(self):
+        ipregex = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+        if self.whoisinfo:
+            self.ips = re.findall(ipregex, self.whoisinfo)
+        return self.ips
 
     def nmap(self, ip_url):
         self.portScaner.scan(ip_url,  arguments="--top-ports 25",sudo=False)
-        pprint(self.portScaner[ip_url])
         return self.portScaner
 
 
@@ -26,32 +32,38 @@ class HythonTools(object):
 class MainApp(object):
     def __init__(self):
         self.tools = HythonTools()
-
+        self.optionlst  = ["Whois query", "Nmap", "Exit"]
+        self.options    = { "Whois query":self.userwhois,
+                            "Nmap"       :self.usernmap}
     def userwhois(self):
         url = raw_input("Input url to get info: ")
-        self.tools.whois(url)
+        pprint(self.tools.whois(url).split("\n"))
 
     def usernmap(self):
         ip = raw_input("Input ip to get info: ")
-        self.tools.nmap(ip)
+        pprint(self.tools.nmap(ip)[ip])
+
+    def userIpsRange(self):
+        pprint(self.tools.loadIps())
+
+
+    def main(self):
+        while True:
+            print "Choose an option"
+            for i,l in enumerate(self.optionlst):
+                print str(i) + ": " + l
+            option = int(raw_input("Wanna use (please input a number between 0 and " + str(len(self.optionlst)-1) + "): "))
+            if option >= 0 and option < len(self.optionlst)-1:
+                f = self.options[self.optionlst[option]]
+                f()
+                continue
+            elif option == len(self.optionlst)-1:
+                print "Bye!!"
+                break
+            else:
+                print "Incorrect option, please try again"
+                continue
 
 if __name__ == "__main__":
     app = MainApp()
-    optionlst = ["Whois query", "Nmap", "Exit"]
-    options = {"Whois query":app.userwhois,
-               "Nmap"       :app.usernmap}
-    while True:
-        print "Choose an option"
-        for i,l in enumerate(optionlst):
-            print str(i) + ": " + l
-        option = int(raw_input("Wanna use (please input a number between 0 and " + str(len(optionlst)-1) + "): "))
-        if option >= 0 and option < len(optionlst)-1:
-            f = options[optionlst[option]]
-            f()
-            continue
-        elif option == len(optionlst)-1:
-            print "Bye!!"
-            break
-        else:
-            print "Incorrect option, please try again"
-            continue
+    app.main()
